d793c47392676eb2abd0cf1aef8e776e4f279ebb
Yordanos tibebu <yordanostibebu5@gmail.com>
2024-08-13 12:07:59 -0700

    updated submit page

--- src/Pages/Submit.tsx
+++ src/Pages/Submit.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import TextField from "@mui/material/TextField";
 function Submit() {
   return (
     <div className="flex flex-col items-center px-6 py-8 mx-auto lg:py-0 mt-24">
@@ -8,51 +8,20 @@ function Submit() {
           <h1 className="text-xl font-semibold  text-gray-900 md:text-2xl use">
             Contact us and get details on your email quickly
           </h1>
-          <form className="space-y-4 md:space-y-6 ">
-            <div className="relative items-center">
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Your Name
-                </label>
-                <div className="flex">
-                  <div className="fas fa-user text-gray-700 border border-gray-300 rounded-s-lg bg-gray-50 p-3 items-center"></div>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-e-lg block w-full p-2.5"
-                    placeholder="Enter your name"
-                    required
-                  />
-                </div>
-              </div>
+          <form className=" space-y-6 ">
+            <div className="space-y-3">
+              <TextField
+                label="Enter name"
+                variant="standard"
+                className="w-full "
+              />
+              <TextField
+                label="Enter email"
+                variant="standard"
+                className="w-full "
+              />
             </div>
-            <div className="relative items-center">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Your email
-                </label>
-                <div className="flex">
-                  <div className="fas fa-envelope text-gray-700 border border-gray-300 rounded-s-lg bg-gray-50 p-3 items-center"></div>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-e-lg block w-full p-2.5"
-                    placeholder="Enter email"
-                    required
-                  />
-                </div>
-              </div>
-            </div>
-
-            <button className="w-full text-white bg-purple-950 hover:opacity-90 font-medium rounded-lg  px-5 py-2.5 text-center">
+            <button className="mt-5 w-full text-white bg-purple-950 hover:opacity-90 font-medium rounded-lg  px-5 py-2.5 text-center">
               Submit
             </button>
             <p className="text-sm  text-gray-600">
